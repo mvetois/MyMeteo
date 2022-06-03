7aba0843331bc63d4b70d83e4a6fb2d0cf369cbe
Matthieu VETOIS <matthieu.vetois@epitech.eu>
2022-06-03 18:49:12 +0200

    Add : Adding CORS and DB Initialization

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,15 +1,26 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 
 import SwaggerOptions from "./utils/swagger";
 import API from "./routes/index";
 
+import initDb from "./utils/initDb";
+
 const app : Express = express();
 const port : number = 5000;
 
 app.listen(port, () => {
     console.log("Server is running on port " + port);
+});
+
+initDb();
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.append("Access-Control-Allow-Origin", "*");
+    res.append("Access-Control-Allow-Methods", "GET,POST");
+    res.append("Access-Control-Allow-Headers", "Content-Type");
+    next();
 });
 
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(SwaggerOptions)));
