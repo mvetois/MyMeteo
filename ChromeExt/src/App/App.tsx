bef5debeb17da7faad4c9e4911d9929ee29e5902
Matthieu VETOIS <matthieu.vetois@epitech.eu>
2022-06-25 12:42:52 +0200

    Add : Add page when api is offline, they redirect on an error page

--- ChromeExt/src/App/App.tsx
+++ ChromeExt/src/App/App.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+import Error from "../Error/Error";
 
 const App = () => {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>Edit <code>src/App/App.tsx</code> and save to reload.</p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >Learn React</a>
-            </header>
-        </div>
-    );
+    const [isLoading, setIsLoading] = useState(true);
+    const [isOnline, setIsOnline] = useState(false);
+
+    axios.get("https://mymeteo.mvetois.fr/api/").then(() => {
+        setIsOnline(true);
+        setIsLoading(false);
+    }).catch(() => {
+        setIsOnline(false);
+        setIsLoading(false);
+    });
+    if (isLoading) {
+        return (
+            <div className="loader"></div>
+        );
+    }
+    return (<>
+        {isLoading ? <div className="loader"></div> : !isOnline ? <Error /> :
+            <div className="App">
+                <header className="App-header">
+                    <p>Edit <code>src/App/App.tsx</code> and save to reload.</p>
+                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >Learn React</a>
+                </header>
+            </div>
+        }
+    </>);
 }
 
 export default App;
+
---
+++ ChromeExt/src/Error/Error.tsx
@@ -0,0 +1,13 @@
+import react from "react";
+
+const Error = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Une erreur est survenue, veuillez réessayer plus tard</p>
+            </header>
+        </div>
+    );
+}
+
+export default Error;
